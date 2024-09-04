b76b5c218d545cb073f6789ecbaef8eef7e10864
Honey Rhose Petil <117594114+honeyrhosepetil@users.noreply.github.com>
2024-09-04 22:31:47 +0800

    Updated Register

---
+++ Activity1/loginScreen/screens/LogIn.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ECF9FC', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 30, 
+  },
+});
